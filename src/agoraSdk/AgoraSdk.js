82a88ac5595d429ec23b512792dfd036b7a5655a
Aman kumar <arup.chandra@jumkey.com>
2021-05-28 10:14:48 +0530

    User can get added

--- src/agoraSdk/AgoraSdk.js
+++ src/agoraSdk/AgoraSdk.js
@@ -8,6 +8,7 @@ let rtc = {
   uid:null,
   // For the local audio track.
   localAudioTrack: null,
+  localVideoTrack:null,
 };
 
 let options = {
@@ -17,7 +18,7 @@ let options = {
   channel: "testing_rtc",
   // Pass a token if your project enables the App Certificate.
   token:
-    "006e32fdcfdb6794ce0a6396d810bcbade4IAAXoegJYlOkImcmkRGBlW3vEXrg37JxKXOT+TZ7BBkMvEjct68AAAAAEABc42rkAFmvYAEAAQCkWK9g",
+    "006e32fdcfdb6794ce0a6396d810bcbade4IAA27VLFYc2OJ5vf+f2rc8QkuQlSHTeIxtpf2qDcx5TGlEjct68AAAAAEAB+PjEWQMCxYAEAAQDjv7Fg",
 };
 
 async function joinChannel() {
@@ -30,11 +31,57 @@ async function joinChannel() {
   console.log("client",rtc.client);
   console.log("uid",rtc.uid);
   console.log("state",rtc.client.connectionState);
+
+  rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+  rtc.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
+
+  await rtc.client.publish([rtc.localAudioTrack, rtc.localVideoTrack]);
+
+  const localPlayerContainer = document.createElement("div");
+  localPlayerContainer.id = rtc.uid;
+  localPlayerContainer.style.width = "640px";
+  localPlayerContainer.style.height = "480px";
+  document.body.append(localPlayerContainer);
+
+  rtc.localVideoTrack.play(localPlayerContainer);
+
+  console.log("publish success!");
+
+  rtc.client.on("user-published", async (user, mediaType) => {
+    await rtc.client.subscribe(user, mediaType);
+    console.log("subscribe success");
+
+    if (mediaType === "video") {
+        const remoteVideoTrack = user.videoTrack;
+        const remotePlayerContainer = document.createElement("div");
+        remotePlayerContainer.textContent = "Remote user " + user.uid.toString();
+        remotePlayerContainer.style.width = "940px";
+        remotePlayerContainer.style.height = "480px";
+        document.body.append(remotePlayerContainer);
+        remoteVideoTrack.play(remotePlayerContainer);
+    }
+
+    if (mediaType === "audio") {
+        const remoteAudioTrack = user.audioTrack;
+        remoteAudioTrack.play();
+    }
+
+    rtc.client.on("user-unpublished", user => {
+        const remotePlayerContainer = document.getElementById(user.uid);
+        remotePlayerContainer.remove();
+    });
+
+});
 }
 
 
 async function leaveChannel() {
   await rtc.client.leave();
+  await rtc.localVideoTrack.setEnabled(false);
+  await rtc.localAudioTrack.setEnabled(false);
+  let myobj = document.getElementById(rtc.uid);
+  myobj.remove();
+  console.log("state",rtc.client.connectionState);
 }
 // async function startBasicCall() {
 //     rtc.client =  AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
